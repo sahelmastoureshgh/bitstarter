e426dca5ebe1ce9e84bbdff025a7e38eddfe2f97
Sahel <sahel.mastoureshgh@gmail.com>
2013-11-14 02:20:35 +0000

    adding new folder

--- web.js
+++ web.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var fs = require('fs');
-require('./main');
-require('./backbone');
-require('./underscore');
-require('./jquery.jgfeed-min');
+var path=require('path');
 var app = express.createServer(express.logger());
-
+app.configure(function(){
+ app.use(express.static(path.join(__dirname,'public')));
+});
 app.get('/', function(request, response) {
   var buf= new Buffer(fs.readFileSync('index.html'),'utf-8');
   response.send(buf.toString());
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
